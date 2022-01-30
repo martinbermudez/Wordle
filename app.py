bf0fe27cd853da1d7cc4db8a57d8adfe3e75fe6c
martinbermudez <martinbermudez97@gmail.com>
2022-01-30 15:13:42 -0300

    Game logic and todo

--- app.py
+++ app.py
@@ -1,32 +1,62 @@
-"""
-wordle.py
-    _ _ _ _ _ _
-    _ _ _ _ _ _
-    _ _ _ _ _ _
-    _ _ _ _ _ _
-    _ _ _ _ _ _
+from colorama import init
+import random
+init()
 
-Tareas:
-    palabra == intento -> ganar.
-    letra en la palabra -> ayuda.
-    letra no esta en la palabra -> no existe.
 
-    palabra dentro de la lista posible de palabras.
-"""
+colors = {
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'grey': "\033[90m",
+    'ENDC': '\033[0m'
+}
+
+def color_letter(letter, color):
+    return colors[color] + letter + colors["ENDC"]
+
+word_list = ["juego", "salto", "cañon", "suelo", "patio"]
 
 # Iniciar el tablero
 print("____________WORDLE!____________")
-word = 'trineo'
+
+win = False
+word = word_list[random.randint(0,len(word_list))]
+print(word)
 board = []
 for i in range(6):
     board.append(['_' for l in range(5)])
 
-text = input('')
-while len(text) != len(word):
-    print(f"la palabra debe tener {len(word)} caracteres.")
+game_loop = 0
+
+while not win and (game_loop < len(word)):
     text = input('')
+    while len(text) != len(word):
+        print(f"la palabra debe tener {len(word)} caracteres.")
+        text = input('')
 
-# Dibujar tablero
-for i in range(6):
-    print(" ".join(board[i]))
+    #win logic
+    if word == text:
+        board[game_loop] = [l for l in text]
+        win = True
+    # letra en la palabra
+    else:
+        test_line = []
+        for j in range(len(text)):
+            if text[j] == word[j]:
+                test_line.append(color_letter(text[j], "green"))
+            elif text[j] in word:
+               test_line.append(color_letter(text[j], "yellow"))
+            else:
+                test_line.append(color_letter(text[j], "grey"))
+         
+        board[game_loop] = test_line
 
+    #Dibujar tablero
+    for i in range(5):
+        print(" ".join(board[i]))
+    game_loop += 1
+
+#Resultado
+if win:
+    print(f"GANASTE! la palabra era {word}.")
+else:
+    print(f"te quedaste sin intentos! la palabra era {word}.")
---
+++ todo.md
@@ -0,0 +1,15 @@
+///WORDLE///
+wordle.py
+    _ _ _ _ _ _
+    _ _ _ _ _ _
+    _ _ _ _ _ _
+    _ _ _ _ _ _
+    _ _ _ _ _ _
+
+Tareas:
+    palabra == intento -> ganar.
+    letra en la palabra -> ayuda.
+    letra no esta en la palabra -> no existe.
+
+    palabra dentro de la lista posible de palabras.
+    dicionario de palabras.
